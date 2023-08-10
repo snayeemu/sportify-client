b2aec8a09b55d08d7226868566ee82dd0ad98b85
snayeemu <snayeemu2@gmail.com>
2023-08-10 16:03:15 +0600

    images changed

--- src/Pages/Home/Slider/MySlider.jsx
+++ src/Pages/Home/Slider/MySlider.jsx
@@ -1,4 +1,7 @@
 import { Carousel } from "flowbite-react";
+import slider1 from "../../../assets/slider-images/slider-1.jpeg";
+import slider4 from "../../../assets/slider-images/slider-4.jpeg";
+import slider5 from "../../../assets/slider-images/slider-5.jpeg";
 
 const MySlider = () => {
   return (
@@ -6,21 +9,9 @@ const MySlider = () => {
       slideInterval={4000}
       className=" h-[calc(100dvh-30dvh)] lg:h-[calc(100dvh-10dvh)]"
     >
-      <img
-        alt="slider-image-1"
-        src="https://www.stmichaelsaldbourne.co.uk/wp-content/uploads/2023/06/IMG_5070-1170x500.jpg"
-        className=" w-full h-full"
-      />
-      <img
-        alt="slider-image-1"
-        src="https://static.vecteezy.com/system/resources/thumbnails/012/001/389/large/soccer-template-design-football-banner-sport-layout-design-green-theme-vector.jpg"
-        className=" w-full h-full"
-      />
-      <img
-        alt="slider-image-1"
-        src="https://media.istockphoto.com/id/1188462138/photo/variety-of-sport-accessories-on-wooden-surface.jpg?s=612x612&w=0&k=20&c=y2l7DYNkxbVteZy-Kx_adCzm-soTRbiEypje4j8ENe0="
-        className=" w-full h-full"
-      />
+      <img alt="slider-image-1" src={slider1} className="w-full h-full" />
+      <img alt="slider-image-1" src={slider4} className="w-full h-full" />
+      <img alt="slider-image-1" src={slider5} className="w-full h-full" />
     </Carousel>
   );
 };
