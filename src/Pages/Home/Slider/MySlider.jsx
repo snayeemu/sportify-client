9e7dea31ecf7157328aee8fe5fbc99ef98b6a7ee
snayeemu <snayeemu2@gmail.com>
2023-08-10 17:48:01 +0600

    slider changed

--- src/Pages/Home/Slider/MySlider.jsx
+++ src/Pages/Home/Slider/MySlider.jsx
@@ -2,17 +2,56 @@ import { Carousel } from "flowbite-react";
 import slider1 from "../../../assets/slider-images/slider-1.jpeg";
 import slider4 from "../../../assets/slider-images/slider-4.jpeg";
 import slider5 from "../../../assets/slider-images/slider-5.jpeg";
+import { Slide } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css";
+
+const spanStyle = {
+  padding: "20px",
+  background: "#efefef",
+  color: "#000000",
+};
+
+const divStyle = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  backgroundSize: "100% 100%",
+};
+const slideImages = [
+  {
+    url: "https://i.ibb.co/54wPCJx/slider-1.jpg",
+    caption: "Slide 3",
+  },
+  {
+    url: "https://i.ibb.co/kSbFK9w/slider-4.jpg",
+    caption: "Slide 1",
+  },
+];
 
 const MySlider = () => {
   return (
-    <Carousel
-      slideInterval={4000}
-      className=" h-[calc(100dvh-30dvh)] lg:h-[calc(100dvh-10dvh)]"
-    >
-      <img alt="slider-image-1" src={slider1} className="w-full h-full" />
-      <img alt="slider-image-1" src={slider4} className="w-full h-full" />
-      <img alt="slider-image-1" src={slider5} className="w-full h-full" />
-    </Carousel>
+    // <Carousel
+    //   slideInterval={4000}
+    //   className=" h-[calc(100dvh-30dvh)] lg:h-[calc(100dvh-10dvh)]"
+    // >
+    //   <img alt="slider-image-1" src={slider1} className="w-full h-full" />
+    //   <img alt="slider-image-1" src={slider4} className="w-full h-full" />
+    //   <img alt="slider-image-1" src={slider5} className="w-full h-full" />
+    // </Carousel>
+    <div className="slide-container">
+      <Slide>
+        {slideImages.map((slideImage, index) => (
+          <div key={index}>
+            <div
+              style={{ ...divStyle, backgroundImage: `url(${slideImage.url})` }}
+              className="h-[60dvh] md:h-[90dvh]"
+            >
+              {/* <span style={spanStyle}>{slideImage.caption}</span> */}
+            </div>
+          </div>
+        ))}
+      </Slide>
+    </div>
   );
 };
 
